ba980e0e425da668f718ad55e62c05d2f1b6f902
GHDBASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-09-06 17:49:54 +0530

    api update

--- OrionXCanteen/frontend/src/services/Api.js
+++ OrionXCanteen/frontend/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // ✅ Use your actual server IP or domain
+  baseURL: 'http://hacktrail-team-orion-x.vercel.app:8000/api', // ✅ Use your actual server IP or domain
 });
 
 // Attach token to requests
