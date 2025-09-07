47cc06485ee76733701aeff079a168be6adbe797
GHDBASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-09-07 14:37:53 +0530

    api links to backend

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // ✅ Use your actual server IP or domain
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api',
 });
+
 
 // Attach token to requests
 api.interceptors.request.use((config) => {
