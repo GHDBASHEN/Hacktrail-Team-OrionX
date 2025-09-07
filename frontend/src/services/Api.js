9f4e6db5d5d6db5a809f65755a20b9c434b8317b
GHDBASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-09-07 19:36:05 +0530

    chore(api): update base URL
    The API endpoint is updated to point to the latest Vercel deployment URL.

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `https://hacktrail-team-orion-x-git-host-ghdbashens-projects.vercel.app/api`,
+  baseURL: `https://hacktrail-team-orion-x-cvvr-git-host-ghdbashens-projects.vercel.app/api`,
 });
 
 
